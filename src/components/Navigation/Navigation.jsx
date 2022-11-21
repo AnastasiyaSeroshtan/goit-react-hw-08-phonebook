219737e732c75a4d9bb1cdd8c72e94cef147d587
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-11-21 16:23:59 +0200

    Update

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,15 @@
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from 'redux/auth/auth-selectors';
 import { NavLinkStyled } from './Navigation.styled';
 
 export const Navigation = () => {
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+
     return (
     <nav>
         <NavLinkStyled to="/" end>Home</NavLinkStyled>
-        <NavLinkStyled  to="/contacts">Contacts</NavLinkStyled>
+        {isLoggedIn &&
+        <NavLinkStyled  to="/contacts">Contacts</NavLinkStyled>}
     </nav>
     )
 };
