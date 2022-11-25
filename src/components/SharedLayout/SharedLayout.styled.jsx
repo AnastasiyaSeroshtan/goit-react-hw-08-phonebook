059abc015950737c6be21ccea89eb544d5771b9b
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-11-26 00:03:25 +0200

    Add max-width

--- src/components/SharedLayout/SharedLayout.styled.jsx
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div` 
+    max-width: 1200px;
     margin: 0 auto;
     padding-left: ${p => p.theme.space[3]}px;
     padding-right: ${p => p.theme.space[3]}px;
-    padding-bottom: ${p => p.theme.space[3]}px;
-    padding-top: ${p => p.theme.space[3]}px;`; 
+    `; 
